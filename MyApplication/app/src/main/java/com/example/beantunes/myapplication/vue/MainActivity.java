e603b58efa8b3a06873dc8aa81bb9ba5a0dee02d
Benjamin Antunes <beantunes@dpt_info.iut.local>
2017-03-22 15:31:17 +0100

    debut alarm

--- MyApplication/app/src/main/java/com/example/beantunes/myapplication/vue/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/beantunes/myapplication/vue/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.beantunes.myapplication.vue;
 
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,17 +18,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.beantunes.myapplication.R;
 import com.example.beantunes.myapplication.metier.ListAlarm;
 
 import org.w3c.dom.Text;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import static java.lang.System.currentTimeMillis;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     public final static String EXTRA_MESSAGE = "truc de fou";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    TimePicker alarmTimePicker;
+    PendingIntent pendingIntent;
+    public AlarmManager alarmManager;
 
 
     private SensorManager mSensorManager;
@@ -52,6 +66,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mRecyclerView.setAdapter(new AlarmAdapter(new ListAlarm().getList()));
 
 
+        //  alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+
 
         //Detection du flip pour cancel l'alarme
             //Chercher la TextView
@@ -63,12 +79,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
     /** Called when the user clicks the Send button */
-    public void sendMessage() {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
+    public void sendMessage(View v) {
+  /*
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+        */
+        long time = currentTimeMillis();
+        Date d1 = new Date();
+        Date d2;
+        //  d2 = new Date(d1.getYear(),d1.getMonth(),d1.getDate(),d1.getHours(),d1.getMinutes(),d1.getSeconds());
+        Toast.makeText(this, "ALARM ON", Toast.LENGTH_SHORT).show();
+        // Calendar calendar = Calendar.getInstance();
+        // calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+        // calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        // Set the alarm's trigger time to 8:30 a.m.
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        calendar.set(Calendar.MINUTE, 58);
+        Toast.makeText(MainActivity.this, "bjr", Toast.LENGTH_SHORT).show();
+        // alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time + 5, 10000, pendingIntent);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        // Bon manque de prevenir le receiver....!!
 
     }
 
